fdf88bfdddebd9ee9c04639502c08e63d11de1b1
lihui <hui.li@cqmfin.com>
2018-05-25 16:57:49 +0800

    builder

---
+++ pattern/src/main/java/com/ldh/pattern/factory/builder/Builder.java
@@ -0,0 +1,10 @@
+package com.ldh.pattern.factory.builder;
+
+/**
+ * Created on 2018/5/24.
+ */
+public interface Builder {
+    public void buildPart1();
+    public void buildPart2();
+    public Product retrieveResult();
+}
