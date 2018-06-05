2c03394c5032689f1c1d88baef98e75a53aef087
lihui <hui.li@cqmfin.com>
2018-06-05 17:51:19 +0800

    工具类

--- utils/src/main/java/com/ldh/utils/comm/CommUtil.java
+++ utils/src/main/java/com/ldh/utils/comm/CommUtil.java
@@ -11,29 +11,32 @@ public class CommUtil {
     /**
      * 将日期格式转换成yyyy-MM-dd的字符串格式
      * 返回值如：2010-10-06
+     *
      * @param time 要转换的日期
      * @return
      */
-    public static  String dateToString(Date time)  {
+    public static String dateToString(Date time) {
 
-        SimpleDateFormat formatter = new  SimpleDateFormat ("yyyy-MM-dd"); //定义将日期格式要换成的格式
-        String stringTime  =  formatter.format(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); //定义将日期格式要换成的格式
+        String stringTime = formatter.format(time);
 
-        return  stringTime;
+        return stringTime;
 
     }
+
     /**
      * 将日期格式转换成yyyyMMdd的字符串格式
      * 返回值如：2010-10-06
+     *
      * @param time 要转换的日期
      * @return
      */
-    public static  String dateTimeToString(Date time)  {
+    public static String dateTimeToString(Date time) {
 
-        SimpleDateFormat formatter = new  SimpleDateFormat ("yyyyMMdd"); //定义将日期格式要换成的格式
-        String stringTime  =  formatter.format(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd"); //定义将日期格式要换成的格式
+        String stringTime = formatter.format(time);
 
-        return  stringTime;
+        return stringTime;
 
     }
 
@@ -41,28 +44,30 @@ public class CommUtil {
     /**
      * 将日期格式转换成yyyy-MM-dd的字符串格式
      * 返回值如：2010-10-06
+     *
      * @param time 要转换的日期
      * @return
      */
-    public static  Date dateToDate(Date time)  {
+    public static Date dateToDate(Date time) {
 
-        SimpleDateFormat formatter = new  SimpleDateFormat ("yyyy-MM-dd"); //定义将日期格式要换成的格式
-        String stringTime  =  formatter.format(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); //定义将日期格式要换成的格式
+        String stringTime = formatter.format(time);
         Date date = null;
         try {
             date = formatter.parse(stringTime);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return  date;
+        return date;
 
     }
 
     /**
      * 得到当前时间，以字符串表示
+     *
      * @return
      */
-    public static String getDate(){
+    public static String getDate() {
         Date date = new Date();
         return CommUtil.dateToString(date);
     }
